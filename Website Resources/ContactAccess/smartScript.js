a685067f8d86ced644c7276432cee30fc61a521a
Olu Olorode <oluolorode27@gmail.com>
2015-12-14 06:21:35 -0500

    Update smartScript.js

--- Website Resources/ContactAccess/smartScript.js
+++ Website Resources/ContactAccess/smartScript.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
   $("#contacts").hide("fast");
   $("#sign_in").show("fast");
-  var clicked = 0;
   $("#button").click(function(){
     $("#contacts").show("fast");
     $("#sign_in").hide("fast"); 
-    clicked = 1;
+    var clicked = 1;
   });
   if (clicked === 1) {
     $("#contacts").show("fast");

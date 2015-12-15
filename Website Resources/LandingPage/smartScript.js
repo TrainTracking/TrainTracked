803342d354dda61453e3b931dd07c6ab8f27f4c7
Olu Olorode <oluolorode27@gmail.com>
2015-12-14 23:32:10 -0500

    Update smartScript.js

--- Website Resources/LandingPage/smartScript.js
+++ Website Resources/LandingPage/smartScript.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $('body').scrollspy({target: "nav", offset: 50});   
 
   // Add smooth scrolling on all links inside the navbar
-  $("#scroll").on('click', function(event) {
+  $("nav #scroll").on('click', function(event) {
 
     // Prevent default anchor click behavior
     event.preventDefault();
